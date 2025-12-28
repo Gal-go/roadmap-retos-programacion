ce2abd729d321b93c7ad32c3e78f127d7e5a5bb7
Gal-go <matiasgabriel1914@gmail.com>
2025-12-28 12:33:46 -0300

    Create Gal-go.js

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Gal-go.js
@@ -0,0 +1,12 @@
+//Esta es la página web oficial que elegí para completar el ejercicio: developer.mozilla.org/es/docs/Web/JavaScript
+
+//Este es un comentario de una sola linea para JavaScript, tiene que utilizdarse "//" para poder comentar en una sola linea en JavaScript.
+
+//Este es un comentario de una sola linea.
+
+/*Este es un comentario
+de más de una sola linea.
+Es preciso "encerrar" lo que comentes
+en este tipo de estructura (asterisco y barra al principio
+y al final)*/
+
